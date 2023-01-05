437d716b52b1fa3739fe0b6466892e874b17d2ec
mfcaicedo <mfcaicedo@unicauca.edu.co>
2023-01-05 14:14:51 -0300

    se agrego el dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import CompetenceForm from './components/CompetenceForm';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      
       {/* Componente de competence */}
-      <CompetenceForm />
+      {/* <CompetenceForm /> */}
+      <Dashboard />
 
     </div>
   );
