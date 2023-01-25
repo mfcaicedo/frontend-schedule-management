1127a69d2249d1bd52da92ecf6209b80ac9a9c6c
Yazmin Gutierrez <yavigutierrez@unicauca.edu.co>
2023-01-25 18:51:00 -0500

    vista formulario Actualizar datos

--- src/components/login/Account.js
+++ src/components/login/Account.js
@@ -1,19 +1,78 @@
+import "../../App.css";
 import { useState, useEffect } from "react"
 import { supabase } from "./supabaseClient"
 import Avatar from './Avatar'
 import PublicRouters from "../../routers/PublicRouters"
+
+//FORMIK
+import { useFormik } from "formik";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { classNames } from "primereact/utils";
+
 //OBTENER VALORES
 const Account = ({ session }) => {
 
     const [loading, setLoading] = useState(true)
     const [username, setUsername] = useState(null)
-    // const [usernameValid, setUsernameValid] = useState(null)
     const [role, setRole] = useState(null)
     const [avatar_url, setAvatarUrl] = useState(null)
+    const [email, setEmail] = useState("");
+    const [showMessage, setShowMessage] = useState(false);
+    const [formData, setFormData] = useState({});
 
     useEffect(() => {
         getProfile()
     }, [session])
+
+    const formik = useFormik({
+        initialValues: {
+            username: '',
+            role: ''
+        },
+        validate: (data) => {
+            let errors = {};
+
+            if (!data.username) {
+                errors.name = 'El nombre es requerido.';
+            }
+
+            if (!data.role) {
+                errors.role = 'El rol es requerido.';
+            }
+            return errors;
+        },
+        onSubmit: (data) => {
+            setFormData(data);
+            setShowMessage(true);
+
+            formik.resetForm();
+        },
+    });
+    const isFormFieldValid = (name) =>
+        !!(formik.touched[name] && formik.errors[name]);
+    const getFormErrorMessage = (name) => {
+        return (
+            isFormFieldValid(name) && (
+                <small className="p-error">{formik.errors[name]}</small>
+            )
+        );
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const { error } = await supabase.auth.signInWithOtp({ email });
+            if (error) throw error;
+            alert("check your email");
+        } catch (error) {
+            console.log(error);
+            alert(error.error_description || error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const getProfile = async () => {
         try {
@@ -27,7 +86,6 @@ const Account = ({ session }) => {
                 .select('username, role, avatar_url')
                 .eq('id', user.id)
                 .single()
-
 
             if (data === null || data.username === null) {
                 setLoading(false)
@@ -45,7 +103,6 @@ const Account = ({ session }) => {
         }
 
     }
-
 
     const updateProfile = async (e) => {
         e.preventDefault()
@@ -76,57 +133,68 @@ const Account = ({ session }) => {
     }
 
     return (
-        <div aria-live="polite" className='container mx-auto'>
-            {console.log("user", username)}
+        <div>
             {loading ? (
                 <PublicRouters />
             ) : (
-                <form onSubmit={updateProfile} className="form-widget">
-                    <Avatar
-                        url={avatar_url}
-                        size={150}
-                        onUpload={(url) => {
-                            setAvatarUrl(url)
-                            updateProfile({ username, role, avatar_url: url })
-                        }}
-                    />
-                    {/* <div>Email: {session.user.email}</div> */}
-                    <div className="container mx-auto w-72 py-4">
-                        <input type="text"
-                            name="text"
-                            id="username"
-                            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                            placeholder="Your Name"
-                            value={username || ''}
-                            onChange={(e) => setUsername(e.target.value)}
-                        />
-                    </div>
-                    <div className="container mx-auto w-72 py-4">
-                        <input type="text"
-                            name="text"
-                            className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
-                            placeholder="coordinador, profesor"
-                            id="role"
-                            value={role || ''}
-                            onChange={(e) => setRole(e.target.value)}
-                        />
-                    </div>
-                    <div className='text-center'>
-                        <button className="w-44 h-11 rounded-full text-gray-50 bg-indigo-600 hover:bg-indigo-700" disabled={loading}>
-                            Update Profile
-                        </button>
-                    </div>
-                    <div className="text-center">
-                        <button type="button" className="button" onClick={() => supabase.auth.signOut()}>
-                            Sign Out
-                        </button>
-                    </div>
-                </form>
-            )}
-
+                <div className="hero">
+                    <div className="contenido-hero">
+                        <div className="form-demo">
+                            <div className="flex justify-content-center">
+                                <div className="card">
+                                    <p
+                                        className="title-form "
+                                        style={{ color: "#FFFFFF", fontWeight: "bold", fontSize: "35px" }}
+                                    >
+                                        Actualizar datos
+                                    </p>
+                                    <p className="text-m text-gray-400 pb-3" style={{ fontSize: "16px" }}>
+                                        Por favor ingrese sus datos:{" "}
+                                    </p>
+                                    <form onSubmit={updateProfile} className="p-fluid">
+                                        <div className="field">
+                                            <span className="p-float-label p-input-icon-right">
+                                                <i className="pi pi-user" />
+                                                <InputText id="username" name="text" value={username || ''} onChange={(e) => setUsername(e.target.value)} autoFocus className={classNames({ 'p-invalid': isFormFieldValid('username') })} />
+                                                <label htmlFor="username" className={classNames({ 'p-error': isFormFieldValid('username') })}>Nombre*</label>
+                                            </span>
+                                            {getFormErrorMessage('username')}
+                                        </div>
+                                        <div className="field">
+                                            <span className="p-float-label p-input-icon-right">
+                                                <i className="pi pi-id-card" />
+                                                <InputText id="text" name="text" value={role || ''} onChange={(e) => setRole(e.target.value)} className={classNames({ 'p-invalid': isFormFieldValid('role') })} />
+                                                <label htmlFor="role" className={classNames({ 'p-error': isFormFieldValid('role') })}>Rol*</label>
+                                            </span>
+                                            {getFormErrorMessage('role')}
+                                        </div>
+                                        <div className="button-footer">
+                                            <Button
+                                                type="submit"
+                                                label="Actualizar datos"
+                                                className="mt-2"
+                                                style={{ background: "#5EB319", borderColor: "#5EB319" }}
+                                                disabled={loading}
+                                            />
+                                            <Button
+                                                type="submit"
+                                                label="Salir"
+                                                className="mt-2"
+                                                style={{ background: "#928E8D", borderColor: "#716F6F" }}
+                                                onClick={() => supabase.auth.signOut()}
+                                            />
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div >
+                    </div >
+                </div >
+            )
+            }
         </div>
+
     )
 }
 
 export default Account;
-//ACTUALIZAR VALORES
